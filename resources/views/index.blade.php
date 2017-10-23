49651d2a7cc5ee5416f2512a45d904bb9c67b004
Vyacheslav Sokolov <anubis-10@mail.ru>
2017-10-23 17:23:31 +0600

    Change color/background in footer. Add "Our Partners" in foter

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,14 +52,30 @@
 
             </div>
         </div>
-        <hr width="80%">
+        <hr>
         <div id="partnersContainer">
             <p>Наши партнеры</p>
-            <div class="img_in_footer">
-
+            <div>
+                <ul class="img_in_footer">
+                    <li>
+                        <a href="http://pfc-cska.com">
+                            <img title="CSKA" alt="CSKA" src="/images/cska.png">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.fclm.ru">
+                            <img title="Lokomotiv" alt="Lokomotiv" src="/images/lokomotiv.png">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.fckrasnodar.ru/">
+                            <img title="Krasnodar" alt="Krasnodar" src="/images/krasnodar.png">
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
-        <hr width="80%">
+        <hr>
         <div id="socialNetworksContainer">
             <p>Мы в социальных сетях</p>
             <div class="img_in_footer">
