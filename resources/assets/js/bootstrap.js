16464030e0bd4e355268fda880ee1c38efdc71a6
Ruslan Tagirov <rusya97@icloud.com>
2017-10-26 15:37:52 +0600

    standart, correct placement

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,17 +1,34 @@
-
-window._ = require('lodash');
-
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
  */
+import './bootstrap';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueMaterial from 'vue-material'
 
-try {
-    window.$ = window.jQuery = require('jquery');
+window.Vue = Vue;
 
-    require('bootstrap-sass');
-} catch (e) {}
+import VueMask from 'v-mask'    // As a plugin
+Vue.use(VueMask);
+
+import { VueMaskDirective } from 'v-mask'   // As a directive
+Vue.directive('mask', VueMaskDirective);
+
+Vue.use(VueRouter);
+Vue.use(VueMaterial);
+
+Vue.material.registerTheme('default',
+    {
+        primary: {
+            color: 'indigo',
+            hue: 300,
+            textColor: 'white'
+        },
+        accent: 'red',
+
+    });
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -36,18 +53,3 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,4 +1,39 @@
 /**
  * Created by ruslantagirov on 14.10.2017.
  */
+import VueRouter from 'vue-router';
 
+let routes = [
+    {
+        mode: 'history',
+        path: '/',
+        components: {
+            board: require('./components/Onboarding.vue'),
+            left: require('./components/LeftSidebar.vue'),
+            right: require('./components/RightSidebar.vue'),
+            footer: require('./components/Footer.vue'),
+            header: require('./components/Header.vue'),
+
+        }
+    },
+    {
+        path: '/reg',
+        components: {
+            header: require('./components/Header.vue'),
+            registration: require('./components/Registration.vue'),
+
+        }
+    },
+    {
+        path: '/login',
+        components: {
+            home: require('./components/Home.vue'),
+            header: require('./components/Header.vue'),
+        }
+    }
+];
+
+export default new VueRouter({
+    routes,
+
+});
