fdf94a59f51c0447536e8f724ffec6706c93e461
Ruslan Tagirov <rusya97@icloud.com>
2017-10-27 18:58:19 +0600

    start work with api

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+     public function signup (Request $request) {      // регистрация пользователя
+
+         $this->validate($request, [
+             'name' => 'required',
+             'surname' => 'required',
+             'patronymic'  => 'required',
+             'sex' => 'required',
+             'birthday' => 'required|date',
+             'country' => 'required',
+             'city' => 'required',
+             'telephone' => 'required',
+             'email' => 'required|email|unique:users',
+             'password' => 'required|max:32'
+         ]);
+
+         $user = new User([
+             'name' => $request->input('name'),
+             'surname' => $request->input('surname'),
+             'patronymic' => $request->input('patronymic'),
+             'sex' => $request->input('sex'),
+             'birthday' => $request->input('birthday'),
+             'country' => $request->input('country'),
+             'city' => $request->input('city'),
+             'telephone' => $request->input('telephone'),
+             'email' => $request->input('email'),
+             'password' => bcrypt($request->input('password'))
+         ]);
+
+         $user->save();
+
+         return response()->json([
+             'message' => 'Successfully, new User created!!!'
+         ], 201);
+     }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('surname');
+            $table->string('patronymic');
+            $table->string('sex');
+            $table->date('birthday');
+            $table->string('country');
+            $table->string('city');
+            $table->string('telephone');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password', 32);
             $table->rememberToken();
             $table->timestamps();
         });
