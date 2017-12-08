d9c6bc76d386e59ac7df3065877a80dd965fdb1e
Ruslan Tagirov <rusya97@icloud.com>
2017-12-08 11:59:47 +0600

    add date column in eventtable

---
+++ database/migrations/2017_12_08_055045_add_column_to_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnToEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->date('date_create')->after('status')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+        });
+    }
+}
