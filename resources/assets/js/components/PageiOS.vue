d89e6cc26945b5d08683017fd4b5fa4e7257dee8
Ruslan Tagirov <rusya97@icloud.com>
2017-12-22 19:01:34 +0600

    helps

--- resources/assets/js/components/PageiOS.vue
+++ resources/assets/js/components/PageiOS.vue
@@ -6,64 +6,64 @@
                     <h1> iOS : </h1>
 					
                     <div>
-                        <img src="/image/ios_1txt.png" alt="ios1t" width="390" height="430">
+                        <img src="/images/ios_1text.png" alt="ios1t" width="390" height="430">
                     </div>
 					
                     <div>
-                        <img src="/image/ios_2txt.png" alt="ios1t" width="390" height="430">
-                        <img src="/image/ios_1.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_2text.png" alt="ios1t" width="390" height="430">
+                        <img src="/images/ios_1.png" alt="ios1" width="390" height="680">
                     </div>
 					
 					<div>
-                        <img src="/image/ios_3txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_2.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_3text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_2.png" alt="ios1" width="390" height="680">
                     </div>
 					
 					<div>
-                        <img src="/image/ios_4txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_3.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_4text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_3.png" alt="ios1" width="390" height="680">
                     </div>
 					
 					<div>
-                        <img src="/image/ios_5txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_4.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_5text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_4.png" alt="ios1" width="390" height="680">
                     </div>
 					<div>
-                        <img src="/image/ios_6txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_5.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_6text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_5.png" alt="ios1" width="390" height="680">
                     </div>
 					<div>
-                        <img src="/image/ios_7txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_6.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_7text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_6.png" alt="ios1" width="390" height="680">
                     </div>
 					<div>
-                        <img src="/image/ios_8txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_7.png" alt="ios1" width="390" height="680">
-                    </div>
-					
-					<div>
-                        <img src="/image/ios_9txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_8.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_8text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_7.png" alt="ios1" width="390" height="680">
                     </div>
 					
 					<div>
-                        <img src="/image/ios_10txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_9.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_9text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_8.png" alt="ios1" width="390" height="680">
                     </div>
 					
 					<div>
-                        <img src="/image/ios_11txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_11.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_10text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_9.png" alt="ios1" width="390" height="680">
                     </div>
 					
 					<div>
-                        <img src="/image/ios_12txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_12.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_11txt.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_11.png" alt="ios1" width="390" height="680">
                     </div>
 					
 					<div>
-                        <img src="/image/ios_13txt.png" alt="ios1t" width="390" height="360">
-                        <img src="/image/ios_13.png" alt="ios1" width="390" height="680">
+                        <img src="/images/ios_12text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_12.png" alt="ios1" width="390" height="680">
+                    </div>
+					
+					<div>
+                        <img src="/images/ios_13text.png" alt="ios1t" width="390" height="360">
+                        <img src="/images/ios_13.png" alt="ios1" width="390" height="680">
                     </div>
 					
                     <div>
